29c566f0ebe0c6fbd3009b2c7927308d062dd44b
Rafał Piotrowski <s421809@pc18k.labs.wmi.amu.edu.pl>
2016-11-19 13:07:55 +0100

    improved NLPgrammars

--- NLPgrammars.py
+++ NLPgrammars.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
+import operator
 
 #część wspólna dwóch list
 def compare(list1,list2):
@@ -8,37 +9,25 @@ def compare(list1,list2):
 
 def action(comp):
     a = comp.index(max(comp))
-    if a==0:
-        return "zatankuj"
-    elif a==1:
-        return "magazyn"
-    elif a==2:
-        return "odbierz"
-    elif a==3:
-        return "zawiez"
-    elif a==4:
-        return "odpocznij"
-    elif a==5:
-        return "pomoz"
+    return a
 
 #laduje z hasla pliku
-file = open('key_words','r')
-zatankuj = file.readline().rstrip().split(',')
-magazyn = file.readline().rstrip().split(',')
-odbierz = file.readline().rstrip().split(',')
-zawiez = file.readline().rstrip().split(',')
-odpocznij = file.readline().rstrip().split(',')
-pomoz = file.readline().rstrip().split(',')
+dict = {}
+file = open('key_words.txt','r')
+content = file.readlines()
+for line in content:
+    line = line.rstrip().split(',')
+    if line[0] not in dict:
+        dict[line[0]] = line[2:]
+    else:
+        dict[line[0]].extend(line[2:])
+print(dict)
 file.close()
 
 while True:
     line = input().split()
-    #lista powtórzeń
-    comp = []
-    comp.append(compare(zatankuj,line))
-    comp.append(compare(magazyn,line))
-    comp.append(compare(odbierz,line))
-    comp.append(compare(zawiez,line))
-    comp.append(compare(odpocznij,line))
-    comp.append(compare(pomoz,line))
-    print(action(comp))
+    comp = {}
+    for key in dict:
+        comp[key] = (compare(dict[key],line))
+    print(comp)
+    print(max(comp, key=comp.get))
