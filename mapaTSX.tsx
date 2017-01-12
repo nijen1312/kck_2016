1af3a57859edffe7b153bdc9c4926c30c84caa42
zwojtyniak <zuzwoj2@st.amu.edu.pl>
2017-01-12 15:33:27 +0100

    Mapa

---
+++ mapaTSX.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="kck4" tilewidth="32" tileheight="32" tilecount="10434" columns="141">
+ <image source="kck4.png" width="4540" height="2396"/>
+</tileset>
