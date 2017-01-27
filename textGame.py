1dc4bdc6b1e0c2c9d79423c5e7e4a48521e0396a
Rafał Piotrowski <s421809@pc01c.labs.wmi.amu.edu.pl>
2017-01-27 10:55:01 +0100

    ostatnia nadzieja

--- textGame.py
+++ textGame.py
@@ -1,7 +1,37 @@
 import sys
+from random import choice
+
+kurier_status = "w pracy"
+
+def magazyn():
+    print(choice())
+    kurier_status = "magazyn"
+def zatankuj():
+    print(choice())
+    kurier_status = "stacja"
+def odbierz(co,ulica):
+    print(choice() + "ulica")
+    kurier_status = "w pracy"
+def zawieź(co,ulica):
+    print(choice() + "ulica")
+    kurier_status = "w pracy"
+def przerwa():
+    print(choice())
+    kurier_status = "przerwa"
 
 while True:
+    print(kurier_status)
     orderLine=sys.stdin.readline()
     orderList=eval(orderLine)
     for order in orderList:
-        print("rozkaz" + str(order))
+        # print("rozkaz" + str(order))
+        if order[0] == "MAGAZYN":
+            magzyn()
+        elif order[0] == "ZATANKUJ":
+            zatankuj()
+        elif order[0] == "ODBIERZ":
+            odbierz(order[1],order[2])
+        elif order[0] == "ZAWIEŹ":
+            zawieź(order[1],order[2])
+        elif order[0] == "PRZERWA":
+            przerwa()
