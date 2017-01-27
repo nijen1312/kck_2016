6de38554a6100546f8dd20fe2a3291c7b6eb7303
Martin <mc.jedynski@outlook.com>
2017-01-27 10:54:18 +0100

    added output

--- textGame.py
+++ textGame.py
@@ -1,7 +1,13 @@
 import sys
 
+odpMagazyn = ["OK, jade na magazyn.","Zjeżdżam na bazę.","Wracam do centrali."]
+odpZatankuj = ["OK, jadę zatankować.", "Jadę nalać do baku."]
+odpPrzerwa = ["OK, jadę sobie odpocząć.","Czas na przerwę !","Nareszczie odpoczynek."]
+odpOdbierz = ["Jadę odebrać paczkę z ","Odbieram paczkę z ","Jadę po paczkę na "]
+odpZawieź = ["Zawożę paczkę na ","Podrzucę paczkę na ", "Dostarczam paczkę na "]
+
 while True:
     orderLine=sys.stdin.readline()
     orderList=eval(orderLine)
     for order in orderList:
-        print("rozkaz" + str(order))
+        # print("rozkaz" + str(order))
